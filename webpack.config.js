68d763d67e5afb43689c1f8eb8be957b4ab1fd05
hakshu <flyhighup.air25@gmail.com>
2020-07-23 11:24:11 +0900

    fix: html読み込み修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
@@ -32,4 +34,9 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
 };
