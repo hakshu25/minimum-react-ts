0cd9311bfd32d31dd5670e4c97e2b051efdc02fd
hakshu <flyhighup.air25@gmail.com>
2020-11-05 19:23:05 +0900

    spec: スナップショットテストを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 
 import './style.scss';
-
-const App = () => <h1>Hello World</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
